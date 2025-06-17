03d737e5fcc6877457bbe50ce244931ab1129b46
danteryung <dantexx@uracle.co.kr>
2025-06-17 16:06:29 +0900

    Initial Commit

---
+++ ActionSampleUITests/ActionSampleUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  ActionSampleUITestsLaunchTests.m
+//  ActionSampleUITests
+//
+//  Created by 정종현 on 5/19/25.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ActionSampleUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation ActionSampleUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+//- (void)testLaunch {
+//    XCUIApplication *app = [[XCUIApplication alloc] init];
+//    [app launch];
+//
+//    // Insert steps here to perform after app launch but before taking a screenshot,
+//    // such as logging into a test account or navigating somewhere in the app
+//
+//    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+//    attachment.name = @"Launch Screen";
+//    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+//    [self addAttachment:attachment];
+//}
+
+@end
